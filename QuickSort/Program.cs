3f84dd6eff2793b1fba5bc3b0210189e4738345b
szymonklo <58526555+szymonklo@users.noreply.github.com>
2020-01-24 20:41:26 +0100

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    //Alternative sorting (counting sorting): https://www.hackerrank.com/challenges/countingsort1/problem
+    // Complete the countingSort function below.
+    static int[] countingSort(int[] arr)
+    {
+        int[] results = new int[100];
+        foreach (int num in arr)
+            results[num]++;
+        return results;
+
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int[] result = countingSort(arr);
+
+        //textWriter.WriteLine(string.Join(" ", result));
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
